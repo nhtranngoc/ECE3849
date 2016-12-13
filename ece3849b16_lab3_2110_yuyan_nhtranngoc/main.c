c33ef3eed8c9c4f40cc7bf6d349ad7473d0683c8
Tran Ngoc <nhtranngoc@wpi.edu>
2016-12-13 02:02:13 -0500

    Finish lm3s2110

--- ece3849b16_lab3_2110_yuyan_nhtranngoc/main.c
+++ ece3849b16_lab3_2110_yuyan_nhtranngoc/main.c
@@ -18,10 +18,18 @@
 
 #include "driverlib/comp.h"
 #include <xdc/runtime/System.h>
-
+#include <xdc/cfg/global.h>
 #include <ti/sysbios/BIOS.h>
 
 #include <ti/sysbios/knl/Task.h>
+
+unsigned long g_timerCount;
+float g_period;
+float g_avgPeriod;
+unsigned int g_intervalCount = 0;
+char g_runOnce = 1;
+unsigned long g_SystemClock;
+unsigned long g_frequency;
 
 void initializeComparator();
 void initializeGPIO();
@@ -30,10 +38,14 @@ void initializeTimer();
 /*
  *  ======== main ========
  */
-Void main() {
+void main() {
+	IntMasterDisable();
+	g_SystemClock = SysCtlClockGet();
 	initializeComparator();
 	initializeGPIO();
 	initializeTimer();
+	NetworkInit();
+	IntMasterEnable();
     BIOS_start();        /* enable interrupts and start SYS/BIOS */
 }
 
@@ -62,4 +74,40 @@ void initializeTimer(){
 	TimerLoadSet(TIMER0_BASE, TIMER_A, 0xffff);
 	TimerIntEnable(TIMER0_BASE, TIMER_CAPA_EVENT);
 	TimerEnable(TIMER0_BASE, TIMER_A);
+}
+
+// Capture Hwi
+void Timer0A_Hwi() {
+	unsigned long prev = 0;
+	TimerIntClear(TIMER0_BASE, TIMER_CAPA_EVENT);
+
+	if(g_runOnce) {
+		g_runOnce = 0;
+		prev = TimerValueGet(TIMER0_BASE, TIMER_A);
+	} else {
+		g_timerCount = TimerValueGet(TIMER0_BASE, TIMER_A);
+		g_period += ((g_timerCount - prev) & 0xffff);
+		g_intervalCount++;
+		prev = g_timerCount;
+	}
+}
+
+// Clock function
+void Clock_ISR() {
+	IArg key;
+
+	key = GateHwi_enter(gateHwi0);
+	unsigned long period = g_period;
+	unsigned long interval = g_intervalCount;
+
+	g_period = 0;
+	g_intervalCount = 0;
+
+	GateHwi_leave(gateHwi0, key);
+
+	g_avgPeriod = (float)period/(float)interval;
+	g_frequency = ((1/g_avgPeriod) * g_SystemClock) * 1000; // Frequency in mHz
+
+	NetworkTx(g_frequency);
+
 }
