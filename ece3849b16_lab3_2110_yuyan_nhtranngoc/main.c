cd2f2ee60f9e0bb7b0cb5bc5b4d6058c6fcd6e21
Uyan <yuyan@wpi.edu>
2016-12-12 20:31:47 -0500

    Lab 3 cont

--- ece3849b16_lab3_2110_yuyan_nhtranngoc/main.c
+++ ece3849b16_lab3_2110_yuyan_nhtranngoc/main.c
@@ -23,21 +23,43 @@
 
 #include <ti/sysbios/knl/Task.h>
 
+void initializeComparator();
+void initializeGPIO();
+void initializeTimer();
 
 /*
  *  ======== main ========
  */
 Void main() {
+	initializeComparator();
+	initializeGPIO();
+	initializeTimer();
+    BIOS_start();        /* enable interrupts and start SYS/BIOS */
+}
+
+void initializeComparator(){
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_COMP0);
 	ComparatorConfigure(COMP_BASE, 0, COMP_TRIG_NONE | COMP_INT_HIGH | COMP_ASRCP_REF | COMP_OUTPUT_NORMAL);
 	ComparatorRefSet(COMP_BASE, COMP_REF_1_5125V);
+}
+
+void initializeGPIO(){
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 	GPIODirModeSet(GPIO_PORTB_BASE, GPIO_PIN_4, GPIO_DIR_MODE_HW); // C0- input
-	GPIOPadConfigSet(GPIO_PORTB_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA,
-	 GPIO_PIN_TYPE_ANALOG);
+	GPIOPadConfigSet(GPIO_PORTB_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_ANALOG);
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
 	GPIODirModeSet(GPIO_PORTD_BASE, GPIO_PIN_7, GPIO_DIR_MODE_HW); // C0o output
 	GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_7, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD);
-	
-    BIOS_start();        /* enable interrupts and start SYS/BIOS */
+}
+
+void initializeTimer(){
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+	GPIODirModeSet(GPIO_PORTB_BASE, GPIO_PIN_0, GPIO_DIR_MODE_HW); // CCP0 input
+	GPIOPadConfigSet(GPIO_PORTB_BASE, GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD);
+	TimerDisable(TIMER0_BASE, TIMER_BOTH);
+	TimerConfigure(TIMER0_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_CAP_TIME);
+	TimerControlEvent(TIMER0_BASE, TIMER_A, TIMER_EVENT_POS_EDGE);
+	TimerLoadSet(TIMER0_BASE, TIMER_A, 0xffff);
+	TimerIntEnable(TIMER0_BASE, TIMER_CAPA_EVENT);
+	TimerEnable(TIMER0_BASE, TIMER_A);
 }
