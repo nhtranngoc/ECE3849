4e92b41cee9c626b06ab00223f42aa2352361aa0
Tran Ngoc <nhtranngoc@wpi.edu>
2016-12-13 03:24:04 -0500

    Finish lab 3

--- ece3849b16_lab3_yuyan_nhtranngoc/network.h
+++ ece3849b16_lab3_yuyan_nhtranngoc/network.h
@@ -10,11 +10,13 @@
 
 #define CAN_BIT_RATE 1000000 // [bps] CAN bit rate, 1 Mbps max
 
-#define MSG_ID_RX 2 // message IDs 11-bit or 29-bit, uncomment for normal ops
-#define MSG_ID_TX 1 // one of the communicating boards should have these swapped
+#define MSG_ID_RX 1 // message IDs 11-bit or 29-bit
+#define MSG_ID_TX 2 // one of the communicating boards should have these swapped
 
 #define MSG_NUM_RX 1 // message number 1...32
 #define MSG_NUM_TX 2 // these can be constant on both communicating boards
+
+extern unsigned long g_frequency;
 
 // CAN interrupt service routine - must be registered with the interrupt controller/dispatcher
 void CAN_ISR(void);
